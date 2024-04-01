f753ec797d9e6f9488b7bc73606289be2bd2aca5
Safi <40953267+5AF1@users.noreply.github.com>
2024-04-01 09:08:32 +0600

    minor update for np_flow_pr

--- evaluate.py
+++ evaluate.py
@@ -119,7 +119,7 @@ def create_flow_submission(model, args, iters=24):
 
         flow_low, flow_pr = model(image1, image2, iters=iters, test_mode=True)
         # np_flow_pr = flow_pr.squeeze().numpy(force = True)
-        np_flow_pr = flow_pr.squeeze().numpy()
+        np_flow_pr = flow_pr.squeeze().cpu().numpy()
 
         flow_file =  flow_file_dir / dataset.image_list[ds_id][0].name
         flow_file.parent.mkdir(exist_ok=True)
