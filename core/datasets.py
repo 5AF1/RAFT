1f1a733e5e7812da7b2b98f2d430f1536e24234d
Safi <40953267+5AF1@users.noreply.github.com>
2024-04-23 10:01:10 +0600

    Update datasets.py valid flag update for top 95 row and bottom 20% rows.

--- core/datasets.py
+++ core/datasets.py
@@ -99,8 +99,9 @@ class SeismicDataset(data.Dataset):
 
         # valid = (flow[0].abs() < 1000).float()
         valid = (flow[0].abs() != 0.0).float()
-        p65 = int(valid.shape[0] * 0.75)
-        valid[p65:] = 0.0
+        p80 = int(valid.shape[0] * 0.80)
+        valid[p80:] = 0.0
+        valid[:95] = 0.0
 
         return pp_data, ps_data, flow, valid
 
