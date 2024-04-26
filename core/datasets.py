8764cbbf7ddef5e3f76290bc366744c4574ee909
Safi <40953267+5AF1@users.noreply.github.com>
2024-04-26 10:19:09 +0600

    back to usual

--- core/datasets.py
+++ core/datasets.py
@@ -100,14 +100,14 @@ class SeismicDataset(data.Dataset):
         flow = torch.from_numpy(flow).permute(2, 0, 1).float()
 
         p80 = int(pp_data.shape[1] * 0.80)
-        flow[:,:95,:]   = 0.0
-        flow[:,p80:,:]  = 0.0
-        pp_data[:,:95,:]  = 0.0
-        ps_data[:,:175,:] = 0.0
+        # flow[:,:95,:]   = 0.0
+        # flow[:,p80:,:]  = 0.0
+        # pp_data[:,:95,:]  = 0.0
+        # ps_data[:,:175,:] = 0.0
 
         # valid = (flow[0].abs() < 1000).float()
         valid = (flow[0].abs() != 0.0).float()
-        # valid[p80:] = 0.0
+        valid[p80:] = 0.0
         # valid[:95] = 0.0
 
         return pp_data, ps_data, flow, valid
